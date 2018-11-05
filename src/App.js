5e7ce1b59b633431e2e70e614f04ade9997871fa
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-11-05 16:04:10 -0600

    google maps api has been added to page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Map from './MapContainer';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hello I am alive!!!!</h1>
+        <div className="mapContainer">
+        <Map />
+        </div>
       </div>
     );
   }
---
+++ src/MapContainer/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+ import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
+
+  class GoogleMapsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: [],
+      selectedPlace: {}
+    }
+    // binding this to event-handler functions
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClick = this.onMapClick.bind(this);
+  }
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+  onMapClick = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
+
+  render() {
+
+    const style = {
+      width: '45%',
+      height: '80%',
+      position: 'relative'
+    }
+    return (
+      <Map
+        item
+        xs = { 12 }
+        style = { style }
+        google = { this.props.google }
+        onClick = { this.onMapClick }
+        zoom = { 14 }
+        initialCenter = {{ lat: 30.2762, lng: -97.7533 }}
+      >
+        <Marker
+          onClick = { this.onMarkerClick }
+          title = { 'Graffiti Park, Austin, TX, USA' }
+          position = {{ lat: 30.2762, lng: -97.7533 }}
+          name = { 'Graffiti Park, Austin, TX, USA' }
+        />
+        <InfoWindow
+          marker = { this.state.activeMarker }
+          visible = { this.state.showingInfoWindow }
+        >
+
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+export default GoogleApiWrapper({
+    apiKey: "AIzaSyAK6X7LO2EiudURnWklG1cqH0yGsnlxgbA"
+})(GoogleMapsContainer)
