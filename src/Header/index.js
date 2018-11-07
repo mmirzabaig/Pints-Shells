e119811d260a1dc26f653257db1d16e9b18c60f9
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-11-07 12:22:11 -0600

    midupdate

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Header } from 'semantic-ui-react';
+import { Route, Switch } from 'react-router-dom';
 
 
 
 const HeaderApp = () => {
   return (
     <Header>
-      <ul>
-        <li>User</li>
-        <li><h1>Pints & Shells</h1></li>
-        <li>Login</li>
-      </ul>
+      // <ul>
+      //   <li>User</li>
+      //   <li><h1>Pints & Shells</h1></li>
+      //   <li>Login</li>
+      // </ul>
+      <Switch>
+          // <Route exact path="/" component={Login}/>
+          <Route exact path="/brewTour" component={}/>
+          <h1>Pints & Shells</h1>
+        </Switch>
     </Header>
     )
 }
