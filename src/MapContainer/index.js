6d40a240817ef1e6e5efae1d957b8031b9366a12
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-11-07 11:18:16 -0600

    button talks to backend

--- src/MapContainer/index.js
+++ src/MapContainer/index.js
@@ -15,7 +15,7 @@ import ReactDOM from "react-dom";
 
   onMarkerClick = (props, marker, e) => {
 
-    // console.log(props, 'HELLO')
+    console.log(props)
     this.setState({
       selectedPlace: props,
       activeMarker: marker,
@@ -35,15 +35,42 @@ import ReactDOM from "react-dom";
 
   onInfoWindowOpen(props, e) {
     const button = (
-      <Button color="green" onClick={e => {
-          console.log("please work");
-        }}
+      <Button color="green" onClick={
+        this.addTour.bind(null, this.state.info)
+        }
       >Add to Brew Tour</Button>
     );
     ReactDOM.render(
       React.Children.only(button),
-      document.getElementById("addTour")
+      document.getElementById("aTour")
     );
+  }
+
+  addTour = async (brewery, e) => {
+    e.preventDefault();
+    let jsonObj = {
+      name: brewery.name,
+      phone: brewery.phone,
+      street: brewery.street,
+      city: brewery.city,
+      state: brewery.state,
+      phone: brewery.phone,
+      website_url: brewery.website_url
+    }
+    console.log(jsonObj, 'MIRZA');
+    try {
+      const addedBrewery = await fetch('http://localhost:9000/brews', {
+        method: 'POST',
+        credentials: 'include',
+        body: JSON.stringify(jsonObj),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    } catch(err){
+      console.log('error')
+      console.log(err)
+    }
   }
 
   render() {
@@ -108,7 +135,7 @@ import ReactDOM from "react-dom";
           </Card.Description>
           </Card.Content>
           <Card.Content extra>
-          <div id="addTour" />
+          <div id="aTour" />
           </Card.Content>
           </Card>
           </div>
